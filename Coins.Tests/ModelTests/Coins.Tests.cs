3f9766c5e153fa8c03cb50bc4943b6f9a8c29864
Kyle Parsons and Ryan Putman <ryan@>
2018-06-27 11:02:06 -0700

    add initial commit and tests for first 4 methods

---
+++ Coins.Tests/ModelTests/Coins.Tests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Counter;
+
+namespace Coins.Tests
+{
+    [TestClass]
+    public class CoinTest
+    {
+        //[TestMethod]
+        //public string UserInput_CreateVariable_String()
+        //{
+        //    User testCoin = new User();
+        //    CollectionAssert.AreEqual(new string{ "60" }, testCoin.UserInput("60"));
+        //}
+
+        [TestMethod]
+        public void Quarters_CreateInteger_int()
+        {
+            User testCoin = new User();
+            Assert.AreEqual(24, testCoin.Quarters(99));
+        }
+        [TestMethod]
+        public void Dimes_CreateInteger_int()
+        {
+            User testCoin = new User();
+            Assert.AreEqual(4, testCoin.Dimes(24));
+        }
+
+        [TestMethod]
+        public void Nickels_CreateInteger_int()
+        {
+            User testCoin = new User();
+            Assert.AreEqual(4, testCoin.Nickels(4));
+        }
+
+        [TestMethod]
+        public void Pennies_CreateInteger_int()
+        {
+            User testCoin = new User();
+            Assert.AreEqual(0, testCoin.Pennies(4));
+        }
+
+    }
+}
---
+++ Coins/Models/Coins.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Counter
+{
+    static class Program
+    {
+        static void Main()
+        {
+
+        }
+    }
+
+    public class User
+    {
+        
+        public string UserInput(string n)
+        {
+            string response = Console.ReadLine();
+            return response;
+        }
+
+        public int Quarters(int number)
+        {
+            int quarters = number / 25;
+            int remainder = number % 25;
+            return remainder;
+        }
+
+        public int Dimes(int QuarterRemainder)
+        {
+            int dimes = QuarterRemainder / 10;
+            int remainder = QuarterRemainder % 10;
+            return remainder;
+        }
+
+        public int Nickels(int DimesRemainder)
+        {
+            int nickels = DimesRemainder / 5;
+            int remainder = DimesRemainder % 5;
+            Console.WriteLine(remainder);
+            return remainder;
+        }
+
+        public int Pennies(int NickelsRemainder)
+        {
+            int pennies = NickelsRemainder / 1;
+            int remainder = NickelsRemainder % 1;
+            Console.WriteLine(remainder);
+            return remainder;
+        }
+    }
+}
